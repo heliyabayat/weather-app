600fd6ca5f264631c0a48060acbf5725f5928fa3
Heliya Bayat <heliyabayat@hotmail.com>
2023-04-11 17:17:14 +0430

    weather APi

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// get first number
+let firstNumber = document.getElementById("firstNumId");
+// get second number
+let secondNumber = document.getElementById("secondNumId");
+// write a function to accept two numbers
+function sumTwoNums(num1, num2) {
+  // add two numbers
+  return num1 + num2;
+}
+
 let now = new Date();
 
 let h2 = document.querySelector("#city-date");
@@ -15,29 +25,59 @@ let month = months[now.getMonth()];
 
 h2.innerHTML = ` ${date} ${month} ${hours}:${min}`;
 
+function displayWeatherCondition(response) {
+  document.querySelector(".city-name").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  let weatherConditionImg = document.querySelector(".top-left__img");
+  weatherConditionImg.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+}
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-text-input");
   // alert("searching");
   // console.log(searchInput.value);
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `search for ${searchInput.value}....`;
+  // let h1 = document.querySelector("h1");
+  // h1.innerHTML = `search for ${searchInput.value}....`;
+  let cityName = document.querySelector("#search-text-input").value;
+  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
 }
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
 
+const unitsEl = document.querySelector(".units");
+const temperatureElement = document.querySelector("#temperature");
+
+function toggleTempType(type) {
+  let currentValue = temperatureElement.innerHTML;
+  let convertedValue = 0;
+  if (type === "celsius") {
+    convertedValue = (currentValue * 9) / 5 + 32;
+  } else {
+    convertedValue = ((currentValue - 32) * 5) / 9;
+  }
+  temperatureElement.innerHTML = Math.round(convertedValue);
+  unitsEl.classList.toggle("celsius");
+  unitsEl.classList.toggle("fahrenheit");
+}
+
 function convertToFahrenheit(event) {
   event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 66;
+  toggleTempType("fahrenheit");
 }
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 function convertToCelsius(event) {
   event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 19;
+  toggleTempType("celsius");
 }
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
